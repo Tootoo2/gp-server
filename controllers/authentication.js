bd90697141d879403c099890e68baa72dd36fba5
Anton <antonstromkarl@gmail.com>
2020-04-20 20:34:35 +0200

    fixed typo when adding users to invitelist

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -17,7 +17,7 @@ exports.signin = function (req, res) {
 exports.signup = (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  const validNames = ["TooToo2, tombarnaby, eldräven"];
+  const validNames = ["TooToo2", "tombarnaby", "eldräven", "PUNKEN"];
 
   const isValid = validNames.filter((name) => name === username);
 
